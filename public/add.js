ea5dfb108f918badf668198da0306a9dc012a578
jagadeesh <jagadeesh.gade@coartha.com>
2023-02-26 08:16:37 +0000

    added

--- public/add.js
+++ public/add.js
@@ -1,29 +1,29 @@
-const form = document.getElementById('add-form');
-const errorMessage = document.getElementById('error-message');
-form.addEventListener('submit', async (event) => {
+// Submit the form when the user clicks the submit button
+const addForm = document.getElementById('addForm');
+addForm.addEventListener('submit', async (event) => {
   event.preventDefault();
-  const nameInput = document.getElementById('name');
-  const ageInput = document.getElementById('age');
-  const name = nameInput.value.trim();
-  const age = parseInt(ageInput.value.trim(), 10);
-  if (!name || !age) {
-    errorMessage.textContent = 'Please provide both name and age';
-    return;
+  event.stopPropagation();
+  if (addForm.checkValidity()) {
+    try {
+      const name = document.getElementById('name').value;
+      const age = document.getElementById('age').value;
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/data', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, age })
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      // Redirect to the home page after submitting the form
+      window.location.href = '/';
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    addForm.classList.add('was-validated');
   }
-  if (isNaN(age)) {
-    errorMessage.textContent = 'Age must be a number';
-    return;
-  }
-  const response = await fetch('/api/data', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ name, age }),
-  });
-  if (!response.ok) {
-    errorMessage.textContent = 'Failed to add data';
-    return;
-  }
-  window.location.href = '/';
 });
